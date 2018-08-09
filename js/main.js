44fb8deac8c18bede9162c8f54679645366cd1a1
Aneesa Saleh <aneesa.saleh@gmail.com>
2018-08-09 20:22:47 +0100

    feat: Make home page responsive on small screens

--- js/main.js
+++ js/main.js
@@ -164,22 +164,26 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const span = document.createElement('span');
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  span.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  span.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  span.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  span.append(more);
+
+  li.append(span);
 
   return li
 }
